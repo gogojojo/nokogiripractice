81d99f0a149efb92223d7921af727dd3c789dd9e
gogojojo <joseph.gochua@gmail.com>
2015-04-02 20:40:22 -0700

    learning how to parse through html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  require 'open-uri'
+  def index
+    doc = Nokogiri::HTML(open("http://boxrec.com/list_bouts.php?human_id=429442&cat=boxer"))
+    @practice = doc.css('li')
+  end
 end
